d2af6aa400af5e145345f2e75fbeff0156817e6b
2023Jado <tuyizjado1994@gmail.com>
2024-06-11 15:52:50 +0200

    removed_SDM_Panthera_tigris(it was a duplication)

--- Panthera_tigris.R
+++ Panthera_tigris.R
@@ -2,31 +2,28 @@
 ###############################################################################
 
 # Install and load the necesary packages
-install.packages("dismo")
-install.packages("biomod2")
 
 library(dismo)
 library(biomod2)
 library(raster)
 library(terra)
 library(rnaturalearth)
+library(rnaturalearthdata)
 library(sf)
 
 # Download the occurrence data for Panthera tigris from GBIF
-
 tiger <- gbif("Panthera ", "tigris*", sp=TRUE)
 
 # change the projection to WGS84
 crs(tiger) <-"epsg:4326" 
 tiger <- vect(tiger)
-tigerdf=as.data.frame(tiger)
-tigerdf
 
 # Read the Nepal shapefile
-nepal <- vect("D:/MSc_EAGLE/Summer_Semester_2024/SpatioTemporal/SDM_Panthera_tigris/SDM_Panthera_tigris/Nepal_shpfile/Nepal_new.gpkg")
+nepal <- vect("C:/Users/Jado/Documents/EAGLE/Semester2/Spatial_modeling/Exam_task/SDM_Panthera_tigris/SDM_Panthera_tigris/Nepal_shpfile/Nepal_new.gpkg")
 
 # clip and plot the occurence data to the country
 tiger <- crop(tiger, nepal)
 plot(nepal)
 plot(tiger, add = TRUE, col = "orange", pch = 19)
 
+# Read bioclimatic data
