8b76f93385b39f2daadfde47780d63ca5ed617f8
2023Jado <tuyizjado1994@gmail.com>
2024-06-20 01:39:43 +0200

    random_forest_added & env data

--- Panthera_tigris.R
+++ Panthera_tigris.R
@@ -8,10 +8,10 @@ library(terra)
 library(rnaturalearth)
 library(rnaturalearthdata)
 library(sf)
-
+library(geodata)
 
 # Download the occurrence data for Panthera tigris from GBIF
-tiger <- gbif("Panthera ", "tigris*", sp=TRUE)
+tiger <- gbif("panthera", "tigris*", sp=TRUE)
 
 # change the projection to WGS84
 crs(tiger) <-"epsg:4326" 
@@ -20,38 +20,81 @@ tiger <- vect(tiger)
 # Read the Nepal shapefile
 nepal <- vect("C:/Users/Jado/Documents/EAGLE/Semester2/Spatial_modeling/Exam_task/SDM_Panthera_tigris/SDM_Panthera_tigris/Nepal_shpfile/Nepal_new.gpkg")
 
-# clip and plot the occurence data to the country
+# clip the occurence data to nepal
 tiger <- crop(tiger, nepal)
-plot(nepal)
-plot(tiger, add = TRUE, col = "orange", pch = 19)
 
-# Read bioclimatic data
-bioclim <- 
 
 # ########### Let's now use Random Forest to model #############
 # ############ the distribution of Panthera tigris #############
 ###############################################################
 library(randomForest)
 
+# Set the working directories
+dirpath <- "C:/Users/Jado/Documents/EAGLE/Semester2/Spatial_modeling/Exam_task/SDM_Panthera_tigris/SDM_Panthera_tigris/Data_ENV/wc2.1_2.5m"
+
+# Download Bioclim data at 2.5 minutes resolution
+# envdata <- rast(worldclim_global(var="bio", res=2.5, path= dirpath))
+
+# List the environmental data from the folder
+envdata <- list.files(dirpath, pattern = ".tif$", full.names = TRUE)
+
+# Read each raster file into a list
+env_list <- sapply(envdata, rast, simplify = FALSE)
+
+# Assign all env data a crs:4326
+env_list <- lapply(env_list, function(x) {crs(x) <- "EPSG:4326"; x})
+
+# Crop each raster to the extent of Nepal using mask
+env_clipped <- lapply(env_list, function(x) mask(x, nepal))
+
+# Add occurrence data and Nepal shapefile to the plot and add the first env layer
+plot(nepal)
+plot(env_clipped[[1]], add=T)
+plot(tiger, add = TRUE, col = "orange", pch = 19)
+
+# Check the names of all env layers
+names(env_clipped)
+
+# Subset the env layers to bio1, bio5, bio6, bio7, bio8, bio12, bio16, bio17 
+# from the env_clipped
+env <- env_clipped[c(1,4,8, 9, 15,16,17, 18)]
+names(env)
+env <- rast(env)
+
+# Rename each layer to a more meaningful name
+names(env) <- c("mean annual air temperature", "annual precipitation amount", "mean monthly precipitation amount of the wettest quarter",
+                "mean monthly precipitation amount of the driest quarter", "Precip.Wettest",
+                "mean daily maximum air temperature of the warmest month", "mean daily minimum air temperature of the coldest month", "annual range of air temperature")
+
+names(env) <- c("MeanTemp.", "AnnualPrecip.", "MeanPrecip.Wettest", "MeanPrecip.Driest", "Precip.Wettest", "MaxTemp.Warmest", "MinTemp.Coldest", "Temp.Range")
+# Set the minimum and maximum longitude and latitude
+xmin <- 80
+xmax <- 89 
+ymin <- 26 
+ymax <- 31 
+
+# Adjust plot to specified extent
+plot(env, xlim = c(xmin, xmax), ylim = c(ymin, ymax))
+
 # Generate some background points for the study area
 set.seed(123)
-back <- spatSample(env, size=1000, as.points=TRUE, method="random", na.rm=TRUE)
+back <- spatSample(env, size=500000, as.points=TRUE, method="random", na.rm=TRUE)
 
-#convert to sf object 
+# convert to sf object 
 back<-st_as_sf(back)
 
-#check number of background points
+# check number of background points
 nrow(back)
 # get environmental covariates at presence locations
-eP<-extract(env,panthera)
+eP<-extract(env,tiger)
 
-#create data frames from values
+# create data frames from values
 Pres.cov<-data.frame(eP,Pres=1)
 
-#Remove the first column which is just an ID field.
+# Remove the first column which is just an ID field.
 Pres.cov<-Pres.cov[,-1]
 
-#check the data
+# check the data
 summary(Pres.cov)
 
 # Remove NA values
@@ -59,13 +102,13 @@ Pres.cov<-Pres.cov[!is.na(Pres.cov$MeanTemp.),]
 
 head(Pres.cov)
 
-#create data frame for background values 
+# create data frame for background values 
 Back.cov<-data.frame(st_drop_geometry(back),Pres=0)
 
-#create data frame for background values 
+# create data frame for background values 
 Back.cov<-data.frame(st_drop_geometry(back),Pres=0)
 
-#combine
+# combine
 all.cov<-rbind(Pres.cov,Back.cov)
 
 # Create a model
@@ -74,43 +117,54 @@ all.cov<-rbind(Pres.cov,Back.cov)
 # The Bioclim() algorithm assess the suitability of a location by comparing 
 # the values of environmental variables at that location to the distribution of 
 # values at known presence locations
-bc_panthera <- bioclim(Pres.cov[,c('MeanTemp.', 'Temp.Range', 'AnnualPrecip.')])
+bc_tiger <- bioclim(Pres.cov[,c('MeanTemp.', 'Temp.Range', 'AnnualPrecip.')])
 
 # Predict the model
-bioclim.map <- predict(env, bc_panthera)
+bioclim.map <- predict(env, bc_tiger)
 
-#plot
-plot(bioclim.map, axes = F, box = F, main = "bioclim: MeanTemp.,Temp.Range,AnnualPrecip.")
+# plot
+plot(bioclim.map, xlim = c(xmin, xmax), ylim = c(ymin, ymax), main = "bioclim: MeanTemp.,Temp.Range,AnnualPrecip.")
 
-# Predict from other bioclimatic variables
-bc_panthera2<-bioclim(Pres.cov[,1:8])
+# Predict from all bioclimatic variables
+bc_tiger2<-bioclim(Pres.cov[,1:8])
 
-bc_panthera2
+bc_tiger2
 
-bioclim.map2 <- predict(env, bc_panthera2)
+# Predict the model with all the bioclimatic variables
+bioclim.map2 <- predict(env, bc_tiger2)
 
-plot(bioclim.map2, axes = F, box = F, main = "bioclimAll")
+plot(bioclim.map2, xlim = c(xmin, xmax), ylim = c(ymin, ymax), main = "bioclimAll")
 
 
 # The randomForest model takes two arguments - parameters that can be tuned 
 # mtry and ntree. mtry refers to the number of predictor variables to try at 
 # each iteration of the model and and ntree specifies the number of trees grown.
-# 500 is the default value often used. Model performance is thought to be more 
+# let's use 500 as the default value which is mostly used. Model performance is 
 # sensitive to mtry than ntree and this can be tuned with the tuneRF function.
 
 
-#tune mtry
+# tune mtry
 tuneRF(x=all.cov[,1:8],y=as.factor(all.cov$Pres)) #checking for model fitting
 
 # If the Out of Box (OOB) error statistic is lowest with an mtry of 1 
 # so, go with that for fitting the model
 
-rf.Panthera<-randomForest(as.factor(Pres)~.,mtry=1,ntree=500,data=all.cov)
+rf.tiger<-randomForest(as.factor(Pres)~.,mtry=1,ntree=500,data=all.cov)
 
 # use the varImpPlot() function to get a measure of variable importance
-varImpPlot(rf.Panthera)
+varImpPlot(rf.tiger)
 
-#create an empty list to hold the results
+# Now for better evaluation, let's divide the data to the testing and training subsets,
+# where in 5 folds, testing:training is 1:4, iteratively.
+
+# set number of folds to use
+folds=5
+
+# presence and absence data according to folds 
+kfold_pres <- kfold(Pres.cov, folds)
+kfold_back <- kfold(Back.cov, folds)
+
+# create an empty list to hold the results
 eRF<-list()
 par(mfrow=c(2,3))
 
@@ -132,24 +186,27 @@ for (i in 1:folds) {
   
 }
 
-#inspect
+# Check the results
 
 eRF
 
-#Get model evaluation statistics:
+# Get model evaluation statistics:
 
 aucRF <- sapply( eRF, function(x){slot(x, 'auc')} )
 
-#Get maxTPR+TNR for the Random Forest model
+# Get maxTPR+TNR for the Random Forest model
 
 Opt_RF<-sapply( eRF, function(x){ x@t[which.max(x@TPR + x@TNR)] } )
 
 Opt_RF
+Mean_OptRF <- mean(Opt_RF)
 
 # Let's predict and map the distribution estimate
-prRF <- predict(env, rf.Panthera,type="prob",index=2)
+prRF <- predict(env, rf.tiger,type="prob",index=2)
 par(mfrow=c(1,2))
-plot(prRF, main='Random Forest Prediction for Panthera tigris in Nepal')
+plot(prRF, xlim = c(xmin, xmax), ylim = c(ymin, ymax), main='Random Forest Prediction for Panthera tigris in Nepal')
 
-plot(prRF > Mean_OptRF, main='presence/absence')
+plot(prRF > Mean_OptRF, xlim = c(xmin, xmax), ylim = c(ymin, ymax), main='presence/absence')
 
+# ############################################################################################
+# ####################### End ###############################################################
